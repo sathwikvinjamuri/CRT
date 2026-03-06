8745749498f84f17a657836961e32e3aea34dc5d
sathwikvinjamuri <vinjamurisathwikkumar>
2026-03-06 13:47:56 +0530

    message

---
+++ IV-SEM/Python/Practice/M02_Logic Building & Patterns/S02/Ps06.py
@@ -0,0 +1,17 @@
+# n = int(input())
+# for i in range(1,n+1):
+#     print(" "*(n-i)+"* "*i)
+# for i in range(n-1,0,-1):
+#     print(" "*(n-i)+"* "*i)
+ 
+'''n = int(input())
+for i in range(1,n+1):
+    for j in range(n-i):
+        print("",end="")
+    for k in range(1,i+1):
+        print(k,end=" ")
+    print()'''
+
+
+
+    
---
+++ IV-SEM/Python/Practice/M03_Core_Components/S01/PS01_Time_Complexity.py
@@ -0,0 +1,9 @@
+'''Time  Complexity:
+
+
+
+
+
+
+
+'''
